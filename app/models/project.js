960e5985a43a9cc26e2622f82f3148bb2ccb436d
Addy Kim <soyaddyk@gmail.com>
2016-07-31 23:18:11 -0700

    Add projectUrl field

--- app/models/project.js
+++ app/models/project.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),	
   description: DS.attr('string'),
+  projectUrl: DS.attr('string'),
   thumbnailUrl: DS.attr('string'),
   timestamp: DS.attr('number')
 });
