853f04ac87427c8cd80cc36cf419090f8f0fb5c8
Addy Kim <soyaddyk@gmail.com>
2016-07-31 22:27:17 -0700

    Make projects submittable

--- app/router.js
+++ app/router.js
@@ -7,8 +7,11 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('page-not-found', { path: '/*wildcard' });
-  this.route('about');
-  this.route('projects');
+
+  this.route('projects', function() {
+  	this.route('new');
+  	// this.route('view', { path: '/:project_id'} );
+  });
 });
 
 export default Router;
