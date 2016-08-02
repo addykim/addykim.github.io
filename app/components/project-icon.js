e56a5899139c969e86342feefe6a9b1cb8d7de24
Addy Kim <soyaddyk@gmail.com>
2016-08-01 20:30:48 -0700

    Change to project-icons

--- app/components/project-icon.js
+++ app/components/project-icon.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	
+  classNames: ['project-preview'],
+
+  showMore: function() {
+  	return true;
+  }.property('project.description')
 });
